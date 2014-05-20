8321e861a2731d4caa3930ce828be8319af39ed0
kazmiero <alex.kazmierowski@gmail.com>
2014-05-20 15:06:54 +0200

    Canonize and probability functions + tests with the dice

--- haskell/Td2.hs
+++ haskell/Td2.hs
@@ -1,6 +1,7 @@
 module Td2 where
 
 import Data.Ratio
+import Data.List
 
 data Prob a = Prob [(a, Ratio Int)]
               deriving (Show)
@@ -11,13 +12,24 @@ instance Functor Prob where
 instance Monad Prob where
   return a = sameProbability [a]
   fail _ = Prob []
-
--- f : (a -> Prob b)
   Prob a >>= f =  Prob $ concat $ map (applyToOne f) a
 
 applyToOne :: (a -> Prob b) -> (a,Ratio Int) -> [(b,Ratio Int)]
 applyToOne f (a,p) = let Prob b = f a
                    in mapSecond (*p) b
+
+-- probability
+probability :: (Ord a, Eq a) => a -> Prob a -> Ratio Int
+probability val (Prob p) = let Prob p1 = canonize (Prob p)
+                           in snd (filter (\x -> val == fst x) p1 !! 0)
+
+-- canonize
+canonize :: (Ord a, Eq a) => Prob a -> Prob a
+canonize (Prob p) = let part = groupBy (\x y -> fst x == fst y) (sort p)
+                  in Prob $ map simplify part
+
+simplify :: [(a, Ratio Int)] -> (a, Ratio Int)
+simplify l = (fst $ l !! 0, foldl1 (+) (snd $ unzip l)) 
 
 -- map functions
 mapFirst :: (a->c) -> [(a,b)] -> [(c,b)] 
@@ -34,7 +46,21 @@ pairList [] [] = []
 pairList (ha:ta) (hb:tb) = (ha,hb) : pairList ta tb
 ---
 
-
 sameProbability :: [a] -> Prob a
 sameProbability list = let len = length list
                            in Prob $ map (\a -> (a,1%len)) list
+--
+dice :: Prob Int
+dice = sameProbability [1,2,3,4,5,6]
+
+double :: Prob Bool
+double = do
+  x <- dice
+  y <- dice
+  return $ x == y
+
+pair :: Prob Int
+pair = do
+  x <- dice
+  y <- dice
+  return $ x + y
